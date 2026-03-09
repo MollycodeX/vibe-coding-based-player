3620c0cf5fc25b753a1443fcee7c1566268e8b29
Mollycode <114975977+MollycodeX@users.noreply.github.com>
2026-03-09 18:32:06 +0000

    Create Playlist.cpp with playlist implementation

---
+++ src/Playlist.cpp
@@ -0,0 +1,33 @@
+// Playlist.cpp
+
+#include "Playlist.h"
+
+Playlist::Playlist() {
+    // Constructor implementation
+}
+
+void Playlist::addSong(const Song& song) {
+    // Add song implementation
+}
+
+void Playlist::removeSong(const std::string& songName) {
+    // Remove song implementation
+}
+
+void Playlist::play() {
+    // Play implementation
+}
+
+void Playlist::pause() {
+    // Pause implementation
+}
+
+void Playlist::stop() {
+    // Stop implementation
+}
+
+void Playlist::shuffle() {
+    // Shuffle implementation
+}
+
+// Additional methods...
